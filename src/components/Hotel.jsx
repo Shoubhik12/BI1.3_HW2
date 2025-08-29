36a2647923b96662471ba30df38b0e3a338a0949
Shoubhik Ghosh <shoubhikghosh360@gmail.com>
2025-08-30 00:03:45 +0530

    feat:New url added

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -2,11 +2,11 @@ import { useState } from "react"
 import useFetch from "../useFetch"
 
 const Hotel=()=>{
-    const {data,loading,error} = useFetch("https://hotels-beryl.vercel.app/hotels")
+    const {data,loading,error} = useFetch("https://hotels-sepia.vercel.app/hotels")
     const [successMessage , setSuccessMessage] = useState("")
     const handleDelete = async (movieId)=>{
         try {
-            const response = await fetch(`https://hotels-beryl.vercel.app/hotels/${movieId}`,{
+            const response = await fetch(`https://hotels-sepia.vercel.app/hotels/${movieId}`,{
                 method:"Delete"
             })
             if(!response.ok){
